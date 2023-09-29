575631ebec3be90d590fcd5234a68c9105af15ec
Archy_k <artbilov@gmail.com>
2023-09-29 11:54:42 -0700

    turns angle diff wip

--- uni/app.js
+++ uni/app.js
@@ -1,15 +1,21 @@
+const { body } = document
+const clock = document.getElementById('clock')
 const [meridiem] = document.getElementsByClassName('meridiem')
 const hourHand = document.getElementById('hour')
 const minuteHand = document.getElementById('min')
 const secondHand = document.getElementById('sec')
 const [dateBlock] = document.getElementsByClassName('date')
 const [yearP, monthP, dateP] = document.querySelectorAll('.date p')
+
 let intervalId
 let customDate
 
 runClock()
 
 dateBlock.onclick = handleChangeDateClick
+hourHand.onmousedown = handleGrab
+onmouseup = handleDrop
+// onclick = handleClick
 
 function runClock() {
   intervalId = setInterval(animateClock, 100)
@@ -18,6 +24,11 @@ function runClock() {
 function animateClock() {
   const date = new Date()
 
+  showDate(date)
+  customDate = date
+}
+
+function showDate(date,) {
   const hoursTurn = (date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()) / 43200
   const minutesTurn = (date.getMinutes() * 60 + date.getSeconds()) / 3600
   const secondsTurn = date.getSeconds() / 60
@@ -25,14 +36,13 @@ function animateClock() {
   const iMonth = date.getMonth()
   const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][iMonth]
 
-  hourHand.style.rotate = hoursTurn + 'turn'
-  minuteHand.style.rotate = minutesTurn + 'turn'
-  secondHand.style.rotate = secondsTurn + 'turn'
+  hourHand.style.rotate = hoursTurn - (hoursTurn > 0.5) + 'turn'
+  minuteHand.style.rotate = minutesTurn - (minutesTurn > 0.5) + 'turn'
+  secondHand.style.rotate = secondsTurn - (secondsTurn > 0.5) + 'turn'
   yearP.innerText = year
   monthP.innerText = month
   dateP.innerText = date.getDate()
   meridiem.innerText = date.getHours() < 12 ? 'AM' : 'PM'
-  customDate = date
 }
 
 function handleChangeDateClick(e) {
@@ -55,7 +65,7 @@ function handleChangeDateClick(e) {
   }
 
   stopClock()
-  showCustomDate()
+  showDate(customDate)
 }
 
 function stopClock() {
@@ -86,17 +96,57 @@ function decrementDay() {
   customDate.setDate(customDate.getDate() - 1)
 }
 
-function showCustomDate() {
-  const year = customDate.getFullYear()
-  const iMonth = customDate.getMonth()
-  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][iMonth]
+function handleGrab(e) {
+  body.style.userSelect = 'none'
 
-  yearP.innerText = year
-  monthP.innerText = month
-  dateP.innerText = customDate.getDate()
+  // const rotateAngOnStartDeg = prevAngle > 0 ? prevAngle * (180 / Math.PI) : (2 * Math.PI + prevAngle) * (180 / Math.PI)
+
+  onmousemove = (e) => {
+  const prevAngle = parseFloat(hourHand.style.rotate)
+  const x0 = clock.offsetLeft + clock.offsetWidth / 2
+  const y0 = clock.offsetTop + clock.offsetHeight / 2
+  const x = e.pageX
+  const y = e.pageY
+  const angle = calculateRotationAngle(x, y, x0, y0)
+  const deltaAngle = angle - prevAngle
+  customDate.setTime(customDate.getTime() + deltaAngle * 3600e3)
+  console.log({ prevAngle, angle, deltaAngle, customDate })
+  showDate(customDate)
+
+  }
+}
+
+function handleClick(e) {
+  const x0 = clock.offsetLeft + clock.offsetWidth / 2
+  const y0 = clock.offsetTop + clock.offsetHeight / 2
+  const x = e.pageX
+  const y = e.pageY
+
+  const angle = calculateRotationAngle(x, y, x0, y0)
+  console.log(angle)
+  // hourHand.style.rotate = angle + 'rad'
+}
+
+function handleDrop() {
+  onmousemove = null
+  body.style.userSelect = null
 }
 
 
+function calculateRotationAngle(x, y, x0, y0) {
+  var deltaX = x - x0
+  var deltaY = y - y0
+  var angleRadians = Math.atan2(deltaX, -deltaY)
+
+  // console.log({ x, y, x0, y0, angleRadians })
+  // var angleDegrees = angleRadians >= 0 ? angleRadians * (180 / Math.PI) : (2 * Math.PI - Math.abs(angleRadians)) * (180 / Math.PI)
+  // var hours = Math.abs(Math.floor(angleDegrees / 30))
+
+
+  // console.log({ aR: angleRadians, aD: angleDegrees, ang0: rotateAngOnStartDeg, h: hours, l: laps,  })
+
+  return angleRadians / (2 * Math.PI)
+}
 
 
 
